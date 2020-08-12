549eb01d840de6ae6ed097ccab70628802a72cef
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-08-12 02:12:57 -0500

    -Added linked list put functions to hashtable

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,11 +1,59 @@
+class Node:
+    def __init__(self, key=None, value=None, next_node=None):
+        self.key = key
+        self.value = value
+        self.next_node = next_node
+
 class HashTableEntry:
     """
     Linked List hash table key/value pair
     """
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next = None
+    def __init__(self, key=None, value=None):
+        self.head = None
+
+    def add_to_head(self, key, value):
+        
+        new_node = Node(key, value)
+        if self.head == None:
+            self.head = new_node
+        
+        else:
+            # new node should point to current head
+            new_node.next_node = self.head
+            # new head equals to new node
+            self.head = new_node
+
+        
+    def contains(self, key):
+        # check if list is empty first, if so return false
+        if self.head is None:
+            return False
+        # loop through each node until we see the key or we could not go further
+        current_node = self.head  
+        while current_node is not None:
+            if current_node.key == key:
+                return True
+            # otherwise go to the next node
+            current_node = current_node.next_node
+        return False
+
+
+    def replace_value(self, key, value):
+        # check if list is empty first, if so return false
+        if self.head is None:
+            return False
+        # loop through each node until we see the key or we could not go further
+        current_node = self.head  
+        while current_node is not None:
+            if current_node.key == key:
+                current_node.value = value 
+            # otherwise go to the next node
+            current_node = current_node.next_node
+        return False
+
+
+
+        
 
 
 # Hash table can't have fewer than this many slots
@@ -16,12 +64,13 @@ class HashTable:
     """
     A hash table that with `capacity` buckets
     that accepts string keys
-
     Implement this.
     """
 
-    def __init__(self, capacity):
-        # Your code here
+    def __init__(self, capacity=8):
+        self.capacity = capacity
+        self.hash_table = capacity * [HashTableEntry()]
+        self.items = 0
 
 
     def get_num_slots(self):
@@ -29,40 +78,30 @@ class HashTable:
         Return the length of the list you're using to hold the hash
         table data. (Not the number of items stored in the hash table,
         but the number of slots in the main list.)
-
         One of the tests relies on this.
-
         Implement this.
         """
-        # Your code here
 
+        return len(self.hash_table)
 
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
-
         Implement this.
         """
-        # Your code here
-
-
-    def fnv1(self, key):
-        """
-        FNV-1 Hash, 64-bit
-
-        Implement this, and/or DJB2.
-        """
-
-        # Your code here
+        return self.items / self.get_num_slots()
 
 
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
-
         Implement this, and/or FNV-1.
         """
-        # Your code here
+   
+        hash = 5381
+        for char in key:
+            hash = (hash * 33) + ord(char)
+        return hash
 
 
     def hash_index(self, key):
@@ -76,23 +115,37 @@ class HashTable:
     def put(self, key, value):
         """
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Implement this.
         """
-        # Your code here
+        # find the index of the key
+        i = self.hash_index(key)
+        # check if key alrady exists
+        # if key does not exist then add key and values to the head
+        if self.hash_table[i].contains(key) == False:
+            # increment item counter 
+            self.item =+ 1
+            return self.hash_table[i].add_to_head(key, value)
+        # else replace the value for the key
+        else:
+            return self.hash_table[i].replace_value(key, value)
 
 
     def delete(self, key):
         """
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Implement this.
         """
-        # Your code here
+        i = self.hash_index(key)
+
+        if self.hash_table[i] == None:
+            print(f"key: {key} Not Found")
+
+        elif self.hash_table[i] != None:
+            print(f"Deleting, value: {repr(self.hash_table[i])}")
+
+        self.hash_table[i] = None
 
 
     def get(self, key):
@@ -103,7 +156,15 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
+        i = self.hash_index(key)
+
+        if self.hash_table[i] == None:
+            print("Key Not Found")
+
+        elif self.hash_table[i] != None:
+            return self.hash_table[i]
+
+
 
 
     def resize(self, new_capacity):
@@ -116,11 +177,23 @@ class HashTable:
         # Your code here
 
 
-
 if __name__ == "__main__":
     ht = HashTable(8)
+    # print(ht.capacity)
+    # print(ht.hash_table[0].head)
 
-    ht.put("line_1", "'Twas brillig, and the slithy toves")
+    print(f"number of buckets:  {ht.get_num_slots()}")
+    # print(ht.hash_index("Twas brillig, and the slithy toves"))
+
+    ht.put("line_1", "Twas brillig, and the slithy toves")
+    ht.put("line_1", "Twas the night before Christmas")
+    print(ht.hash_table[0].head.value)
+    print(ht.hash_table[0].head.key)
+    # print(ht.hash_table[0].head.next_node.value)
+
+    print(f"number of items {ht.items}")
+
+
     ht.put("line_2", "Did gyre and gimble in the wabe:")
     ht.put("line_3", "All mimsy were the borogoves,")
     ht.put("line_4", "And the mome raths outgrabe.")
@@ -132,6 +205,13 @@ if __name__ == "__main__":
     ht.put("line_10", "Long time the manxome foe he sought--")
     ht.put("line_11", "So rested he by the Tumtum tree")
     ht.put("line_12", "And stood awhile in thought.")
+
+    ht.get("line_12")
+
+    ht.delete("line_12")
+    ht.delete("line_12")
+    ht.get("line_12")
+
 
     print("")
 
